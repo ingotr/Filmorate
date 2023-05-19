d98cf83d65c93adb876e10403741f15573bb659c
Aleksandr Markovkin <markovkin88@gmail.com>
2023-05-19 18:36:54 +0300

    15. Обновить валидацию

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,12 +3,14 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Data
 @Builder
 public class Film {
+    @NotBlank
     private final String name;
     private final String description;
     private final Optional<LocalDate> releaseDate;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,12 +3,16 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class User {
+    @Email
     private final String email;
+    @NotBlank
     private final String login;
     private final LocalDate birthday;
     private Integer id;
